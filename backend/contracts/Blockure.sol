5cd9b6c7178771af05ac5f64b32d2ddba9291d95
Shivaji Chalise <respond2shivaji@gmail.com>
2024-05-16 20:20:48 +0545

    removing unwanted code

--- backend/contracts/Blockure.sol
+++ backend/contracts/Blockure.sol
@@ -6,7 +6,7 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract MyToken is ERC721, ERC721URIStorage, Ownable {
+contract Blockure is ERC721, ERC721URIStorage, Ownable {
     uint256 private _nextTokenId;
 
     constructor(address initialOwner)
@@ -21,13 +21,18 @@ contract MyToken is ERC721, ERC721URIStorage, Ownable {
     }
 
     // _beforeTokenTransfer method is a hook that is called before any token transfer
-    function _beforeTokenTransfer(address from, address to, uint256 firstTokenId, uint256 batchSize)
-    internal
-    override
-    virtual {
+    // function _beforeTokenTransfer(address from, address to, uint256 firstTokenId, uint256 batchSize)
+    // internal
+    // override
+    // virtual {
+    //     require(from == address(0), "Err: BK Token transfer is BLOCKED");   
+    //     super._beforeTokenTransfer(from, to, firstTokenId, batchSize);
+    // }
+
+    function _update(address from, address to, uint256 value) internal virtual {
         require(from == address(0), "Err: BK Token transfer is BLOCKED");   
-        super._beforeTokenTransfer(from, to, tokenId, batchSize);
     }
+
 
     // The following functions are overrides required by Solidity.
 
